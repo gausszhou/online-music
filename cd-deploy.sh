4fc76b602e113b8aaf0205dee76760fb475de715
gausszhou <gausszhou@qq.com>
2022-08-10 15:46:52 +0800

    chore: run bash script

--- cd-deploy.sh
+++ cd-deploy.sh
@@ -3,8 +3,5 @@ npm run build
 
 docker build -t netease-cloud-music-vue .
 
-docker run -d \
---name netease-cloud-music-vue \
--p 9001:80 \
-netease-cloud-music-vue
+
 
---
+++ cr-run.sh
@@ -0,0 +1,4 @@
+docker run -d \
+--name netease-cloud-music-vue \
+-p 9001:80 \
+netease-cloud-music-vue
