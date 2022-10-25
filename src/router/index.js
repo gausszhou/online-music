52902e85e96e98ba9bff0c3254e0659e907d3d47
gausszhou <gausszhou@qq.com>
2022-10-25 21:55:32 +0800

    chore: 撤销路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,112 +1,127 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Music from "@/views/music/index.vue";
-import MV from "@/views/mv/index.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Music from '@/views/music/index.vue';
+import MusicRecommend from '@/views/music/recommend.vue';
+import MusicNewSongs from '@/views/music/newsongs.vue';
+import MusicSongLists from '@/views/music/songlists.vue';
+import MusicRankLists from '@/views/music/ranklists.vue';
+
+import Singer from '@/views/singer/singer.vue';
+
+import MV from '@/views/mv/index.vue';
+import MVNew from '@/views/mv/new.vue';
+import MVAll from '@/views/mv/all.vue';
+
+import Search from  '@/views/search/index.vue'
+
+import Detail from '@/views/detail/index.vue';
+import DetailMV from '@/views/detail/detailMV.vue';
+import DetailSongSheet from '@/views/detail/detailSongSheet.vue';
+import DetailSinger from '@/views/detail/detailSinger.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "*",
-    redirect: { name: "recommend" }
+    path: '*',
+    redirect: { name: 'recommend' }
   },
   {
-    name: "music",
-    path: "/music",
+    name: 'music',
+    path: '/music',
     component: Music,
     children: [
       {
-        name: "recommend",
-        path: "recommend",
-        component: () => import(/* webpackChunkName: "route-recommend" */ "@/views/music/recommend.vue"),
-        meta: { title: "发现" }
+        name: 'recommend',
+        path: 'recommend',
+        component: MusicRecommend,
+        meta: { title: '发现' }
       },
       {
-        name: "newsongs",
-        path: "newsongs",
-        component: () => import(/* webpackChunkName: "route-newsongs" */ "@/views/music/newsongs.vue"),
-        meta: { title: "新歌" }
+        name: 'newsongs',
+        path: 'newsongs',
+        component: MusicNewSongs,
+        meta: { title: '新歌' }
       },
 
       {
-        name: "songlists",
-        path: "songlists",
-        component: () => import(/* webpackChunkName: "route-songlists" */ "@/views/music/songlists.vue"),
-        meta: { title: "歌单" }
+        name: 'songlists',
+        path: 'songlists',
+        component: MusicSongLists,
+        meta: { title: '歌单' }
       },
       {
-        name: "ranklists",
-        path: "ranklists",
-        component: () => import(/* webpackChunkName: "route-ranklists" */ "@/views/music/ranklists.vue"),
-        meta: { title: "排行" }
+        name: 'ranklists',
+        path: 'ranklists',
+        component: MusicRankLists,
+        meta: { title: '排行' }
       }
     ]
   },
   {
-    name: "singer",
-    path: "/singer",
-    component: () => import(/* webpackChunkName: "route-singer" */  "@/views/singer/singer.vue"),
-    meta: { title: "歌手" }
+    name: 'singer',
+    path: '/singer',
+    component: Singer,
+    meta: { title: '歌手' }
   },
   {
-    name: "mv",
-    path: "/mv",
+    name: 'mv',
+    path: '/mv',
     component: MV,
     children: [
       {
-        name: "newMV",
-        path: "newMV",
-        component: () => import(/* webpackChunkName: "route-newMV" */ "@/views/mv/newMV.vue"),
-        meta: { title: "最新MV" }
+        name: 'newMV',
+        path: 'newMV',
+        component: MVNew,
+        meta: { title: '最新MV' }
       },
       {
-        name: "allMV",
-        path: "allMV",
-        component: () => import(/* webpackChunkName: "route-allMV" */ "@/views/mv/allMV.vue"),
-        meta: { title: "全部MV" }
-      },
-    
+        name: 'allMV',
+        path: 'allMV',
+        component: MVAll,
+        meta: { title: '全部MV' }
+      }
     ]
   },
   {
-    name: "search",
-    path: "/search",
-    component: () => import("@/views/search/index.vue"),
-    meta: { title: "搜索结果|" }
+    name: 'search',
+    path: '/search',
+    component: Search,
+    meta: { title: '搜索结果|' }
   },
   {
-    name: "detail",
-    path: "/detail",
-    component: () => import("@/views/detail/index.vue"),
+    name: 'detail',
+    path: '/detail',
+    component: Detail,
     children: [
       {
-        name: "detailMV",
-        path: "detailMV",
-        component: () => import("@/views/detail/detailMV.vue"),
-        meta: { title: "MV详情" }
+        name: 'detailMV',
+        path: 'detailMV',
+        component: DetailMV,
+        meta: { title: 'MV 详情' }
       },
       {
-        name: "detailSongSheet",
-        path: "detailSongSheet",
-        component: () => import("@/views/detail/detailSongSheet.vue"),
-        meta: { title: "歌单详情" }
+        name: 'detailSongSheet',
+        path: 'detailSongSheet',
+        component: DetailSongSheet,
+        meta: { title: '歌单详情' }
       },
       {
-        name: "detailSinger",
-        path: "detailSinger",
-        component: () => import("@/views/detail/detailSinger.vue"),
-        meta: { title: "歌手详情" }
+        name: 'detailSinger',
+        path: 'detailSinger',
+        component: DetailSinger,
+        meta: { title: '歌手详情' }
       }
     ]
   }
 ];
 
-const router = new VueRouter({  
-  mode: "hash", //  history  hash
-  base: "/online-music/",
+const router = new VueRouter({
+  mode: 'hash', //  history  hash
+  base: '/online-music/',
   routes
 });
-
 
 // 导出路由
 export default router;
--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,8 @@ module.exports = {
   assetsDir:'static',
   // 配置相对路径
   publicPath: process.env.NODE_ENV === "production" ? "/online-music/" : "/",
-  // 取消生成map文件
+  // 生成map文件
   productionSourceMap: false,
-  // 去除文件哈希值
-  filenameHashing: false
+  // 文件哈希值
+  filenameHashing: true
 };
